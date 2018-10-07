19ea1fd9d84855c8cb71c657d0b88d9dc7a6a1a1
udaykumar4923 <udaykumar@qainfotech.com>
2018-10-07 12:23:55 +0530

    initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+ export default function(){
+  return [
+    { title: 'Javascript',pages:200},
+    { title: 'Harry Potter', pages:300},
+    { title: 'Game of Throne',pages:400},
+    { title: 'ruby',pages:250}
+  ]
+}
